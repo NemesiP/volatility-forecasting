e190466eb22e9001e58bba84d99b3af3b6122c58
Peter Nemesi <peter.nemesi@hotmail.hu>
2020-11-24 16:10:43 +0100

    Example.ipynb and extended weight method were added

--- Model/main.py
+++ Model/main.py
@@ -17,10 +17,40 @@ class Weights():
     def x_weighted(self, x, params):
         self.theta1, self.theta2 = params
         w = self.weights(x.shape[1])
-        xw = np.matrix(x*w).sum(axis = 1)
+        xw = x*w
         df = pd.DataFrame(xw)
         return df.iloc[:, 0].values
+
+class WeightMethod():
+    def __init__(self):
+        self.theta = []
+        
+    def BetaWeights(self, params, nlags):
+        self.theta.append(params[0])
+        self.theta.append(params[1])
+        eps = np.spacing(1)
+        x = np.linspace(eps, 1.0 - eps, nlags)
+        beta_vals = beta.pdf(x, self.theta[0], self.theta[1])/sum(beta.pdf(x, self.theta[0], self.theta[1]))
+        return beta_vals
     
+    def x_weighted_beta(self, x, params):
+        w = WeightMethod().BetaWeights(params, x.shape[1])
+        xw = np.matmul(x, w)
+        return xw
+    
+    def ExpAlmonWeights(self, params, nlags):
+        self.theta.append(params[0])
+        self.theta.append(params[1])
+        ith = np.arange(1, nlags + 1)
+        almon_vals = np.exp(self.theta[0] * ith + self.theta[1] * ith ** 2)
+        almon_vals = almon_vals / sum(almon_vals)
+        return almon_vals
+    
+    def x_weighted_almon(self, y, params):
+        w = WeightMethod().ExpAlmonWeights(params, x.shape[1])
+        xw = np.matmul(x, w)
+        return xw
+
 class Lags():
     def buildLag(s, lag=2, dropna=True):
         if type(s) is pd.DataFrame:
@@ -34,7 +64,7 @@ class Lags():
         elif type(s) is pd.Series:
             the_range = range(lag+1)
             res = pd.concat([s.shift(i) for i in the_range], axis = 1)
-            red.columns = ['lag_%d' %i for i in the_range]
+            res.columns = ['lag_%d' %i for i in the_range]
         else:
             print('Only works for DataFrame or Series')
             return None
