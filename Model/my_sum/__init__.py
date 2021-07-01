311b8eed0fb81f89a505dc74dae10590d1bb3ae3
NemesiP <71609800+NemesiP@users.noreply.github.com>
2021-07-01 11:11:48 +0200

    Delete Model directory

--- Model/my_sum/__init__.py
+++
@@ -1,9 +0,0 @@
-class calculate():
-    def __init__(self, data):
-        self.data = data
-        
-    def sum(self):
-        total = 0
-        for val in self.data:
-            total += val
-        return total
